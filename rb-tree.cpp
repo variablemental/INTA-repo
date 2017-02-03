1d869025f73f15119a6e85b2f5e25c66c96d0e4c
coder-z <346145732@qq.com>
2017-02-03 23:53:17 +0800

    RED-BLACK Tree structure is done and the function insert is finished

--- rb-tree.cpp
+++ rb-tree.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #define MAXS(a,b) a>b?a:b
+#define BLACK 1
+#define RED   0
 using namespace std;
 
 
@@ -9,6 +11,7 @@ typedef struct Node
 	Node *left;
 	Node *right;
 	Node *parent;
+	int color;
 	Node(int data=0,Node *left=NULL,Node *right=NULL,Node *parent=NULL){
 		this->data=data;
 		this->left=left;
@@ -24,6 +27,7 @@ private:
 	int height;
 	int calHeight(Node *node);
 protected:
+	void setRoot(Node *node){root=node;};
 	void transplant(Node *u,Node *v);	
 public:
 	Tree(Node *root=NULL):root(root){}
@@ -143,20 +147,158 @@ int Tree::getHeight()
 	return calHeight(root);
 }
 
+class rb_tree:public Tree
+{
+private:
+	Node *nil;
+protected:
+	void right_rotate(Node *node);
+	void left_rotate(Node *node);
+	void setRoot(Node *node){Tree::setRoot(node);getRoot()->parent=nil;}
+public:
+	rb_tree(Node *root):Tree(root){
+		nil=new Node(0,NULL,NULL,NULL);
+		nil->color=BLACK;
+		root->color=BLACK;
+		root->parent=nil;
+		root->left=nil;
+		root->right=nil;
+	}
+	void Insert(int data);
+	void TranverseInorder(Node *node);
+		
+	
+
+};
+
+void rb_tree::right_rotate(Node *node)
+{
+	if(node==nil)
+		return;
+	Node *y=node->left;
+	node->left=y->right;
+	if(y->right!=nil)
+		y->right->parent=node;
+	y->parent=node->parent;
+	if(node->parent==nil)
+		setRoot(y);
+	else if(node==node->parent->left)
+		node->parent->left=y;
+	else
+		node->parent->right=y;
+	y->right=node;
+	node->parent=y;
+}
+
+void rb_tree::left_rotate(Node *node)
+{
+	if(node==nil)
+		return;
+	Node *y=node->right;
+	node->right=y->left;
+	if(y->left!=nil)
+		y->left->parent=node;
+	y->parent=node->parent;
+	if(node->parent==nil)
+		setRoot(y);
+	else if(node==node->parent->left)
+		node->parent->left=y;
+	else 
+		node->parent->right=y;
+	y->left=node;
+	node->parent=y;
+}
+
+void rb_tree::Insert(int data)
+{
+	Node *z=new Node(data,NULL,NULL,NULL);
+	Node *x=getRoot();
+	Node *y=nil;
+	while(x!=nil){
+		y=x;
+		if(z->data<x->data)
+			x=x->left;
+		else
+			x=x->right;
+	}
+	z->parent=y;
+	if(y==nil)
+		setRoot(z);
+	else if(y->data>z->data)
+		y->left=z;
+	else y->right=z;
+	z->left=nil;
+	z->right=nil;
+	z->color=RED;
+	
+	while(z->parent->color==RED){
+		if(z->parent==z->parent->parent->left){
+			Node *uncle=z->parent->parent->right;
+			if(uncle->color==RED){
+				z->parent->color=BLACK;
+				uncle->color=BLACK;
+				z->parent->parent->color=RED;
+				z=z->parent->parent;
+			}
+			else if(z==z->parent->right){
+				z=z->parent;
+				left_rotate(z);
+			}
+			else{
+				z->parent->color=BLACK;
+				z->parent->parent->color=RED;
+				right_rotate(z->parent->parent);
+			}
+		}
+		else{
+			Node *uncle=z->parent->left;
+			if(uncle->color==RED){
+				z->parent->color=BLACK;
+				uncle->color=BLACK;
+				z->parent->parent->color=RED;
+			}
+			else if(z==z->parent->left){
+				z=z->parent;
+				right_rotate(z);
+			}
+			else
+			{
+				z->parent->color=BLACK;
+				z->parent->parent->color=RED;
+				left_rotate(z->parent->parent);
+			}
+		}
+	}
+}
+
+void rb_tree::TranverseInorder(Node *node)
+{
+	if(node!=nil){
+		TranverseInorder(node->left);
+		cout<<node->data<<endl;
+		TranverseInorder(node->right);
+	}
+}
 
 int main()
 {
 	int arr[6]={7,5,2,5,7,8};
 	Node *root=new Node(6,NULL,NULL,NULL);
 	Tree *t=new Tree(root);
-	for(int i=0;i<sizeof(arr)/sizeof(typeof(arr[0]));i++){
-		t->Insert(t->getRoot(),arr[i]);
+//	for(int i=0;i<sizeof(arr)/sizeof(typeof(arr[0]));i++){
+//		t->Insert(t->getRoot(),arr[i]);
+//	}
+//	t->TranverseInorder(t->getRoot());
+//	t->Delete(t->Query(t->getRoot(),7));
+//	cout<<endl;
+//	t->TranverseInorder(t->getRoot());
+//	cout<<t->getHeight()<<endl;
+	Node *rb_root=new Node(10,NULL,NULL,NULL);
+	rb_tree *t2=new rb_tree(rb_root);
+		for(int i=0;i<sizeof(arr)/sizeof(typeof(arr[0]));i++){
+		t2->Insert(arr[i]);
 	}
-	t->TranverseInorder(t->getRoot());
-	t->Delete(t->Query(t->getRoot(),7));
-	cout<<endl;
-	t->TranverseInorder(t->getRoot());
-	cout<<t->getHeight()<<endl;
+	t2->TranverseInorder(t2->getRoot());
 	return 0;
 }
 
